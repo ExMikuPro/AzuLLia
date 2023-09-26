b1239002d70bf3a752e7112dc7b5f260682c2d9e
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-09-26 14:03:12 +0800

    feat: 添加页面函数

--- service/page.go
+++ service/page.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
 )
 
@@ -11,5 +12,84 @@ func NoFoundPage(c *gin.Context) { // 设置404页面
 		Msg:  "页面走丢了~",
 		Path: c.Request.URL.Path,
 		Data: nil,
+	})
+}
+
+func aboutPage(context *gin.Context) { // 关于界面
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: gin.H{
+			"version": Version,
+		},
+	})
+}
+
+func paperListPage(context *gin.Context) { // 获取文章列表
+	// todo 添加api认证
+	data := ReadAllDB(DataBase, "paperList")
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: gin.H{
+			"length": len(data), // 列表长度
+			"list":   data,      // 列表内容
+		},
+	})
+}
+
+func paperContextPage(context *gin.Context) { // 获取文章内容
+	from := context.Query("from") // 文章对应的内容 todo 使用数据库原生id
+	// cid := context.Query("cid") // 文章cid
+
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: gin.H{
+			"isExist": true,          // todo 判断文章是否存在
+			"from":    from,          // 查找对应的文章
+			"context": "<p>文章主题</p>", // todo 文章主体内容
+		},
+	})
+}
+
+func tagListPage(context *gin.Context) { // 标签列表
+	data := ReadAllDB(DataBase, "tagList")
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: gin.H{
+			"length": len(data), // 列表长度
+			"list":   data,      // 列表数据
+		},
+	})
+}
+
+func tagPage(context *gin.Context) { // 查询单个标签内容
+	filter := bson.D{
+		{"tid", context.Param("tid")}, // tid为String
+	}
+	data := ReadOneDB(DataBase, "tagList", filter)
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: data,
+	})
+}
+
+func testPage(context *gin.Context) { // 设置临时测试界面
+	context.JSON(http.StatusOK, GeneralJSONHeader{
+		Code: SuccessCode,
+		Msg:  "success",
+		Path: context.Request.URL.Path,
+		Data: gin.H{
+			"message": "如你所见这是个测试界面awa",
+			"code":    verifyCode(15),
+		},
 	})
 }
--- service/type.go
+++ service/type.go
@@ -9,8 +9,26 @@ const ErrorSession = 2       // 错误的连接
 const ErrorPermissions = 500 // 权限错误
 const WorkProgress = 520     // 正在构建的界面
 
+const seed = 15 //  生成随机认证码
+
 const DBAddress = "mongodb://localhost:27017" // 数据库地址
 const DBDataBase = "test"                     // 数据库名称
+
+var pageAddr = map[string]string{ // 路由地址
+	// 组地址
+	"data": "/data/", // 内容获取函数组
+	"api":  "/api/",  // 列表获取函数组
+	// 页面地址
+	// data组
+	"paperContext": "/paperContext/:cid", // 文章内容
+	"tag":          "/tag/:tid",
+	// api组
+	"tagList":   "/tagList",   // 标签列表
+	"paperList": "/paperList", // 文章列表
+	// 系统页面地址
+	"about":  "/about",  // 关于框架界面
+	"verify": "/verify", // 获取认证界面
+}
 
 type GeneralJSONHeader struct { // 全局通用标头
 	Code int    `json:"code"`
@@ -20,6 +38,8 @@ type GeneralJSONHeader struct { // 全局通用标头
 }
 
 var RequiredHeaders = map[string]string{ // 验证服务
-	//todo 编写认证码生成器
-	"Verify": "dfVndFuvKbJnSdOKe_9517",
+	// 生成随机数
+	"Verify": verifyCode(seed),
 }
+
+var DataBase = InitDB() // 初始化数据库
