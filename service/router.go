5584a424eff81499c672a81bd840046000124515
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-10-18 20:35:29 +0800

    重写主页页面函数

--- service/router.go
+++ service/router.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func routerMain(router *gin.Engine) {
@@ -11,30 +10,23 @@ func routerMain(router *gin.Engine) {
 	api := router.Group(pageAddr["api"])
 
 	api.GET(pageAddr["paperList"], basicPages.PostListPage) // 全部文章列表
-	api.GET(pageAddr["tagList"], basicPages.tageListPage)   // 文章标签列表
+	api.GET(pageAddr["tagList"], basicPages.TageListPage)   // 文章标签列表
 
 	data.GET(pageAddr["paperContext"], basicPages.PostContextPage) // 文章内容
-	data.GET(pageAddr["tag"], basicPages.tagePage)                 // 文章标签内容
+	data.GET(pageAddr["tag"], basicPages.TagePage)                 // 文章标签内容
 
 	add := data.Group("/add/") // 添加相关函数
 	add.POST("/tage")          // 添加标签接口 todo 添加身份认证
 
 	// 通用型路由组
-	router.GET("/", func(context *gin.Context) { // 主页
-		context.JSON(http.StatusOK, GeneralJSONHeader{
-			Code: WorkProgress,
-			Msg:  "页面正在施工喵～",
-			Path: context.Request.URL.Path,
-			Data: nil,
-		})
-	})
-	router.GET(pageAddr["about"], basicPages.aboutPage) // 关于界面
+	router.GET("/", basicPages.MainPage)                // 主页页面
+	router.GET(pageAddr["about"], basicPages.AboutPage) // 关于界面
 
 	router.GET(pageAddr["verify"]) // api认证申请接口
 
 	router.POST("/upload", UploadFile) // 配置上传界面位置
 
-	router.GET("/test", basicPages.aboutPage) // 测试界面
+	router.GET("/test", basicPages.AboutPage) // 测试界面
 }
 
 func Router(router *gin.Engine) {
