9cc4175bc8d76c7154be14997f589a626439bd55
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-10-18 19:45:32 +0800

    将页面函数改写到结构体中

--- service/page.go
+++ service/page.go
@@ -6,33 +6,32 @@ import (
 	"net/http"
 )
 
-func NoFoundPage(c *gin.Context) { // 设置404页面
-	c.JSON(http.StatusNotFound, GeneralJSONHeader{
-		Code: http.StatusNotFound,
-		Msg:  "页面走丢了~",
-		Path: c.Request.URL.Path,
-		Data: nil,
-	})
-}
-
-func aboutPage(context *gin.Context) { // 关于界面
-	context.JSON(http.StatusOK, GeneralJSONHeader{
+func (_ *BasicPage) aboutPage(ctx *gin.Context) { // 框架版本界面
+	ctx.JSON(http.StatusOK, GeneralJSONHeader{
 		Code: SuccessCode,
 		Msg:  "success",
-		Path: context.Request.URL.Path,
+		Path: ctx.Request.URL.Path,
 		Data: gin.H{
 			"version": Version,
 		},
 	})
 }
 
-func paperListPage(context *gin.Context) { // 获取文章列表
-	// todo 添加api认证
+func (_ *BasicPage) NoFoundPage(ctx *gin.Context) { // 404
+	ctx.JSON(http.StatusNotFound, GeneralJSONHeader{
+		Code: http.StatusNotFound,
+		Msg:  "页面走丢了~",
+		Path: ctx.Request.URL.Path,
+		Data: nil,
+	})
+}
+
+func (_ *BasicPage) PostListPage(ctx *gin.Context) { // 文章列表界面
 	data := ReadAllDB(DataBase, "paperList")
-	context.JSON(http.StatusOK, GeneralJSONHeader{
+	ctx.JSON(http.StatusOK, GeneralJSONHeader{
 		Code: SuccessCode,
 		Msg:  "success",
-		Path: context.Request.URL.Path,
+		Path: ctx.Request.URL.Path,
 		Data: gin.H{
 			"length": len(data), // 列表长度
 			"list":   data,      // 列表内容
@@ -40,14 +39,15 @@ func paperListPage(context *gin.Context) { // 获取文章列表
 	})
 }
 
-func paperContextPage(context *gin.Context) { // 获取文章内容
-	from := context.Query("from") // 文章对应的内容 todo 使用数据库原生id
+func (_ *BasicPage) PostContextPage(ctx *gin.Context) { // 文章内容页面
+	from := ctx.Query("from") // 文章对应的内容 todo 使用数据库原生id
 	// cid := context.Query("cid") // 文章cid
+	// todo 建立文章数据表
 
-	context.JSON(http.StatusOK, GeneralJSONHeader{
+	ctx.JSON(http.StatusOK, GeneralJSONHeader{
 		Code: SuccessCode,
 		Msg:  "success",
-		Path: context.Request.URL.Path,
+		Path: ctx.Request.URL.Path,
 		Data: gin.H{
 			"isExist": true,          // todo 判断文章是否存在
 			"from":    from,          // 查找对应的文章
@@ -56,12 +56,12 @@ func paperContextPage(context *gin.Context) { // 获取文章内容
 	})
 }
 
-func tagListPage(context *gin.Context) { // 标签列表
+func (_ *BasicPage) tageListPage(ctx *gin.Context) { // 标签列表界面
 	data := ReadAllDB(DataBase, "tagList")
-	context.JSON(http.StatusOK, GeneralJSONHeader{
+	ctx.JSON(http.StatusOK, GeneralJSONHeader{
 		Code: SuccessCode,
 		Msg:  "success",
-		Path: context.Request.URL.Path,
+		Path: ctx.Request.URL.Path,
 		Data: gin.H{
 			"length": len(data), // 列表长度
 			"list":   data,      // 列表数据
@@ -69,37 +69,24 @@ func tagListPage(context *gin.Context) { // 标签列表
 	})
 }
 
-func tagPage(context *gin.Context) { // 查询单个标签内容
+func (_ *BasicPage) tagePage(ctx *gin.Context) {
 	filter := bson.D{
-		{"tid", context.Param("tid")}, // tid为String
+		{"tid", ctx.Param("tid")}, // tid为String
 	}
 	data, isExist := ReadOneDB(DataBase, "tage", filter)
 	if isExist { // 判断标签是否存在
-		context.JSON(http.StatusOK, GeneralJSONHeader{
+		ctx.JSON(http.StatusOK, GeneralJSONHeader{
 			Code: SuccessCode,
 			Msg:  "success",
-			Path: context.Request.URL.Path,
+			Path: ctx.Request.URL.Path,
 			Data: data,
 		})
 	} else {
-		context.JSON(http.StatusOK, GeneralJSONHeader{
+		ctx.JSON(http.StatusOK, GeneralJSONHeader{
 			Code: NoDocuments,
 			Msg:  "DB No Documents",
-			Path: context.Request.URL.Path,
+			Path: ctx.Request.URL.Path,
 			Data: data,
 		})
 	}
-
-}
-
-func testPage(context *gin.Context) { // 设置临时测试界面
-	context.JSON(http.StatusOK, GeneralJSONHeader{
-		Code: SuccessCode,
-		Msg:  "success",
-		Path: context.Request.URL.Path,
-		Data: gin.H{
-			"message": "如你所见这是个测试界面awa",
-			"code":    verifyCode(15),
-		},
-	})
 }
