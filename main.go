9274e9353ff94d7fef3ee6c6e442b9e34bf75134
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-10-23 20:00:49 +0800

    设置默认上传数据最大值

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	router := gin.Default() // 注册默认静态路由
 	// router.Use(service.AuthMiddleware(service.RequiredHeaders))
-	service.Router(router)     // 注册路由服务器
-	service.StaticFile(router) // 注册静态资源服务器
-	err := router.Run(":82")   // 启动服务器
+	router.MaxMultipartMemory = 8 << 20 // 设置默认上传大小限制为8MB
+	service.Router(router)              // 注册路由服务器
+	service.StaticFile(router)          // 注册静态资源服务器
+	err := router.Run(":82")            // 启动服务器
 	if err != nil {
 		fmt.Println("Server Start ERROR:", err)
 		return
