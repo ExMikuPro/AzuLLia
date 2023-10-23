a9c7af1f1aecbce84bc1461a46533d8c15d672f6
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-10-23 20:39:42 +0800

    添加发行版架构相关设置

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// gin.SetMode(gin.ReleaseMode) // 设置模式为发行模式
+	// gin.DefaultWriter = ioutil.Discard // 关闭控制台输出
 	router := gin.Default() // 注册默认静态路由
 	// router.Use(service.AuthMiddleware(service.RequiredHeaders))
 	router.MaxMultipartMemory = 8 << 20 // 设置默认上传大小限制为8MB
