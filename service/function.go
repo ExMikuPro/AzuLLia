927e8251e70e11ca5744bc6234df81ae0db03a09
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-07 17:21:09 +0800

    启用```golangci-lint```规范化代码

--- service/function.go
+++ service/function.go
@@ -4,40 +4,10 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
-	"math/rand"
 	"net/http"
 )
 
 // 中间件
-
-func AuthMiddleware(requiredHeaders map[string]string) gin.HandlerFunc { // 校验请求的参数
-	return func(context *gin.Context) {
-		for key, value := range requiredHeaders {
-			if context.GetHeader(key) != value {
-				context.JSON(http.StatusUnauthorized,
-					GeneralJSONHeader{
-						Code: ErrorPermissions,
-						Msg:  "Permissions ERROR",
-						Path: context.Request.URL.Path,
-						Data: nil,
-					})
-				context.Abort()
-				return
-			}
-		}
-		context.Next()
-	}
-}
-
-func verifyCode(long int64) string { // 随机生成验证码
-	var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-
-	b := make([]rune, long)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
-	}
-	return string(b)
-}
 
 func (_ *Utility) UserPasswdVerify(data userLoginData) bool { // 用户密码认证函数
 	// userHAS, err := bcrypt.GenerateFromPassword([]byte(data.Username), bcrypt.DefaultCost) // 生成用户名的HAS值
@@ -45,7 +15,7 @@ func (_ *Utility) UserPasswdVerify(data userLoginData) bool { // 用户密码认
 	//	return false
 	//}
 
-	DbData, have := dBService.ReadOneDB(DataBase, "user", bson.D{{"name", data.Username}})
+	DbData, have := dBService.ReadOneDB(DataBase, "user", bson.D{bson.E{Key: "name", Value: data.Username}})
 	if have {
 		utility.Log("is:", DbData)
 		if DbData["name"] != data.Username {
@@ -60,7 +30,7 @@ func (_ *Utility) UserPasswdVerify(data userLoginData) bool { // 用户密码认
 
 func (_ *Utility) CheckLoginMiddleware() gin.HandlerFunc { // 通过cookie认证
 	return func(ctx *gin.Context) {
-
+		// cookie, err := ctx.Cookie("")
 	}
 }
 
