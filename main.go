367a60bd60df908991e0d636497386adbba5774c
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-07 15:21:25 +0800

    添加 Swagger 文档 & 编写部分文档

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
+	_ "AzuLLia/docs"
 	"AzuLLia/service"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"io"
 	"os"
 )
 
+// @title AzuLLia
+// @version 1.0.1206
+// @description 一只轻量级无头CMS
+// @host 127.0.0.1:8080
+// @BasePath /
 func main() {
 	if os.Getenv("RunMode") != "" {
-		gin.SetMode(gin.ReleaseMode)       // 设置模式为发行模式
-		gin.DefaultWriter = ioutil.Discard // 关闭控制台输出
+		gin.SetMode(gin.ReleaseMode)   // 设置模式为发行模式
+		gin.DefaultWriter = io.Discard // 关闭控制台日志输出
 	}
+
 	router := gin.Default() // 注册默认静态路由
 	// router.Use(service.AuthMiddleware(service.RequiredHeaders))
 	router.MaxMultipartMemory = 8 << 20 // 设置默认上传大小限制为8MB
 	service.Router(router)              // 注册路由服务器
 	service.StaticFile(router)          // 注册静态资源服务器
+	if os.Getenv("RunMode") == "" {     // 如歌哦巍峨
+		router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+	}
 	if os.Getenv("ServerPort") != "" {
 		err := router.Run(os.Getenv("ServerPort")) // 启动服务器
 		if err != nil {
