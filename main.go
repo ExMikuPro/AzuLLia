5987f492cb0619901d568f38cd1de85a55a1f2ec
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-12 22:39:14 +0800

    添加并使用环境变量配置文件

--- main.go
+++ main.go
@@ -7,8 +7,6 @@ import (
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-	"io"
-	"os"
 )
 
 // @title AzuLLia
@@ -17,29 +15,17 @@ import (
 // @host 127.0.0.1:8080
 // @BasePath /
 func main() {
-	if os.Getenv("RunMode") != "" {
-		gin.SetMode(gin.ReleaseMode)   // 设置模式为发行模式
-		gin.DefaultWriter = io.Discard // 关闭控制台日志输出
-	}
-
-	router := gin.Default() // 注册默认静态路由
-	// router.Use(service.AuthMiddleware(service.RequiredHeaders))
-	router.MaxMultipartMemory = 8 << 20 // 设置默认上传大小限制为8MB
-	service.Router(router)              // 注册路由服务器
-	service.StaticFile(router)          // 注册静态资源服务器
-	if os.Getenv("RunMode") == "" {     // 如歌哦巍峨
+	service.EvnLoad()                       // 初始化环境
+	gin.SetMode(service.GetEvn("GIN_MODE")) //  设置运行模式
+	router := gin.Default()                 // 注册默认静态路由
+	router.MaxMultipartMemory = 8 << 20     // 设置默认上传大小限制为8MB
+	service.Router(router)                  // 注册路由服务器
+	service.StaticFile(router)              // 注册静态资源服务器
+	if gin.Mode() == "debug" {              // 开发文档
 		router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	}
-	if os.Getenv("ServerPort") != "" {
-		err := router.Run(os.Getenv("ServerPort")) // 启动服务器
-		if err != nil {
-			fmt.Println("Server Start ERROR:", err)
-		}
-	} else {
-		err := router.Run(":8080") // 启动测试服务器
-		if err != nil {
-			fmt.Println("Server Start ERROR:", err)
-		}
+	err := router.Run(service.GetEvn("WEBSITE_PORT")) // 启动服务器
+	if err != nil {
+		fmt.Println("Server Start ERROR:", err)
 	}
-
 }
