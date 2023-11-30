803f2773340db69c9006ab2b28d6c70ceea07954
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-30 20:07:40 +0800

    fix: 修改API路由地址及请求方法

--- service/router.go
+++ service/router.go
@@ -6,42 +6,39 @@ import (
 
 func routerMain(router *gin.Engine) {
 	// 功能型接口路由组
-	listGroup := router.Group("/list/")         // 列表
-	getGroup := router.Group("/get/")           // 内容
-	adminGroup := router.Group("/admin/")       // 管理
-	addGroup := adminGroup.Group("/add/")       // 添加
-	updateGroup := adminGroup.Group("/update/") // 更新
-	deleteGroup := adminGroup.Group("/delete/") // 删除
+	adminGroup := router.Group("/admin/") // 管理
+	apiGroup := router.Group("/api/")     // 外部调用接口
+	v1Group := apiGroup.Group("/v1/")     // 版本号
 
 	// 添加中间键
 	router.Use(utilityFunction.ReturnHeader())              // 响应头中间件
 	adminGroup.Use(utilityFunction.CheckLoginMiddleware())  // 登陆认证
 	adminGroup.Use(utilityFunction.verifyHeaderLoginCode()) // 请求头认证
 
-	listGroup.GET("/article", getFunction.ArticleList)   // 文章列表
-	listGroup.GET("/tag", getFunction.TagList)           // 标签列表
-	listGroup.GET("/category", getFunction.CategoryList) // 分类列表
+	v1Group.GET("/article", getFunction.ArticleList)   // 文章列表
+	v1Group.GET("/tag", getFunction.TagList)           // 标签列表
+	v1Group.GET("/category", getFunction.CategoryList) // 分类列表
 
-	getGroup.GET("/article/:id", getFunction.GetArticle)   // 文章内容
-	getGroup.GET("/tag/:id", getFunction.GetTag)           // 标签内容
-	getGroup.GET("/category/:id", getFunction.GetCategory) // 标签内容
+	v1Group.GET("/article/:id", getFunction.GetArticle)   // 文章内容
+	v1Group.GET("/tag/:id", getFunction.GetTag)           // 标签内容
+	v1Group.GET("/category/:id", getFunction.GetCategory) // 标签内容
 
-	addGroup.POST("/tag", addFunction.AddTag)           // 添加标签
-	addGroup.POST("/category", addFunction.AddCategory) // 添加分类
-	addGroup.POST("/group", addFunction.AddGroup)       // 添加用户组
-	addGroup.POST("/user", addFunction.AddUser)         // 添加用户
-	addGroup.POST("/article", addFunction.AddArticle)   // 添加文章
+	v1Group.POST("/tag", addFunction.AddTag)           // 添加标签
+	v1Group.POST("/category", addFunction.AddCategory) // 添加分类
+	v1Group.POST("/group", addFunction.AddGroup)       // 添加用户组
+	v1Group.POST("/user", addFunction.AddUser)         // 添加用户
+	v1Group.POST("/article", addFunction.AddArticle)   // 添加文章
 
-	updateGroup.POST("/tag", updateFunction.UpdateTag)          // 更新标签
-	updateGroup.POST("/category", updateFunction.UpdateArticle) // 更新类别
-	updateGroup.POST("/group", updateFunction.UpdateGroup)      // 更新用户组
-	updateGroup.POST("/user", updateFunction.UpdateUser)        // 更新用户
+	v1Group.PUT("/tag", updateFunction.UpdateTag)          // 更新标签
+	v1Group.PUT("/category", updateFunction.UpdateArticle) // 更新类别
+	v1Group.PUT("/group", updateFunction.UpdateGroup)      // 更新用户组
+	v1Group.PUT("/user", updateFunction.UpdateUser)        // 更新用户
 
-	deleteGroup.POST("/tag", deleteFunction.DeleteTag)           // 删除标签
-	deleteGroup.POST("/category", deleteFunction.DeleteCategory) // 删除类别
-	deleteGroup.POST("/group", deleteFunction.DeleteGroup)       // 删除用户组
-	deleteGroup.POST("/user", deleteFunction.DeleteUser)         // 删除用户
-	deleteGroup.POST("/article", deleteFunction.DeleteArticle)   // 删除文章
+	v1Group.DELETE("/tag", deleteFunction.DeleteTag)           // 删除标签
+	v1Group.DELETE("/category", deleteFunction.DeleteCategory) // 删除类别
+	v1Group.DELETE("/group", deleteFunction.DeleteGroup)       // 删除用户组
+	v1Group.DELETE("/user", deleteFunction.DeleteUser)         // 删除用户
+	v1Group.DELETE("/article", deleteFunction.DeleteArticle)   // 删除文章
 	// 通用型路由组
 	router.GET("/", getFunction.MainPage) // 主页
 
