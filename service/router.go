f135201ccbce56694f0e8d2893e33b164514a4d6
ExMikuPro <72144156+ExMikuPro@users.noreply.github.com>
2023-11-30 20:35:18 +0800

    fix: 删除admin路由组&为数据操作函数添加中间件

--- service/router.go
+++ service/router.go
@@ -6,14 +6,11 @@ import (
 
 func routerMain(router *gin.Engine) {
 	// 功能型接口路由组
-	adminGroup := router.Group("/admin/") // 管理
-	apiGroup := router.Group("/api/")     // 外部调用接口
-	v1Group := apiGroup.Group("/v1/")     // 版本号
+	apiGroup := router.Group("/api/") // 外部调用接口
+	v1Group := apiGroup.Group("/v1/") // 版本号
 
 	// 添加中间键
-	router.Use(utilityFunction.ReturnHeader())              // 响应头中间件
-	adminGroup.Use(utilityFunction.CheckLoginMiddleware())  // 登陆认证
-	adminGroup.Use(utilityFunction.verifyHeaderLoginCode()) // 请求头认证
+	router.Use(utilityFunction.ReturnHeader()) // 响应头中间件
 
 	v1Group.GET("/article", getFunction.ArticleList)   // 文章列表
 	v1Group.GET("/tag", getFunction.TagList)           // 标签列表
@@ -23,23 +20,23 @@ func routerMain(router *gin.Engine) {
 	v1Group.GET("/tag/:id", getFunction.GetTag)           // 标签内容
 	v1Group.GET("/category/:id", getFunction.GetCategory) // 标签内容
 
-	v1Group.POST("/tag", addFunction.AddTag)           // 添加标签
-	v1Group.POST("/category", addFunction.AddCategory) // 添加分类
-	v1Group.POST("/group", addFunction.AddGroup)       // 添加用户组
-	v1Group.POST("/user", addFunction.AddUser)         // 添加用户
-	v1Group.POST("/article", addFunction.AddArticle)   // 添加文章
+	v1Group.POST("/tag", utilityFunction.verifyHeaderLoginCode(), addFunction.AddTag)           // 添加标签
+	v1Group.POST("/category", utilityFunction.verifyHeaderLoginCode(), addFunction.AddCategory) // 添加分类
+	v1Group.POST("/group", utilityFunction.verifyHeaderLoginCode(), addFunction.AddGroup)       // 添加用户组
+	v1Group.POST("/user", utilityFunction.verifyHeaderLoginCode(), addFunction.AddUser)         // 添加用户
+	v1Group.POST("/article", utilityFunction.verifyHeaderLoginCode(), addFunction.AddArticle)   // 添加文章
 
-	v1Group.PUT("/tag", updateFunction.UpdateTag)         // 更新标签
-	v1Group.PUT("/article", updateFunction.UpdateArticle) // 更新文章
-	v1Group.PUT("/group", updateFunction.UpdateGroup)     // 更新用户组
-	v1Group.PUT("/user", updateFunction.UpdateUser)       // 更新用户
-	v1Group.PUT("/category")                              // 更新分类
+	v1Group.PUT("/tag", utilityFunction.verifyHeaderLoginCode(), updateFunction.UpdateTag)         // 更新标签
+	v1Group.PUT("/article", utilityFunction.verifyHeaderLoginCode(), updateFunction.UpdateArticle) // 更新文章
+	v1Group.PUT("/group", utilityFunction.verifyHeaderLoginCode(), updateFunction.UpdateGroup)     // 更新用户组
+	v1Group.PUT("/user", utilityFunction.verifyHeaderLoginCode(), updateFunction.UpdateUser)       // 更新用户
+	v1Group.PUT("/category")                                                                       // 更新分类
 
-	v1Group.DELETE("/tag", deleteFunction.DeleteTag)           // 删除标签
-	v1Group.DELETE("/category", deleteFunction.DeleteCategory) // 删除类别
-	v1Group.DELETE("/group", deleteFunction.DeleteGroup)       // 删除用户组
-	v1Group.DELETE("/user", deleteFunction.DeleteUser)         // 删除用户
-	v1Group.DELETE("/article", deleteFunction.DeleteArticle)   // 删除文章
+	v1Group.DELETE("/tag", utilityFunction.verifyHeaderLoginCode(), deleteFunction.DeleteTag)           // 删除标签
+	v1Group.DELETE("/category", utilityFunction.verifyHeaderLoginCode(), deleteFunction.DeleteCategory) // 删除类别
+	v1Group.DELETE("/group", utilityFunction.verifyHeaderLoginCode(), deleteFunction.DeleteGroup)       // 删除用户组
+	v1Group.DELETE("/user", utilityFunction.verifyHeaderLoginCode(), deleteFunction.DeleteUser)         // 删除用户
+	v1Group.DELETE("/article", utilityFunction.verifyHeaderLoginCode(), deleteFunction.DeleteArticle)   // 删除文章
 
 	v1Group.POST("/userLogin", userFunction.UserLogin) // 用户登陆
 
